d1395136190ad7954b4b7667cfc858a5ef1ea753
zarth01 <zarith.nino@gmail.com01>
2024-08-13 16:08:31 -0500

    preparacion examen

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card mensaje="Hola, soy una tarjeta con props!" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = ({ mensaje }) => {
+  return (
+    <div>{ mensaje }</div>
+  )
+}
+
+export default Card
